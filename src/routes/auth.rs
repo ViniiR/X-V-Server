ffb27dc6f2b1763c6abca4a2761bd2db2934c1a6
ViniiR <cvrm2004@gmail.com>
2025-02-23 11:45:59 -0300

    [FIX] remove unnecessary query on auth/validate

--- src/routes/auth.rs
+++ src/routes/auth.rs
@@ -5,23 +5,23 @@ use rocket::{
 
 use crate::{
     auth::validate_jwt,
-    database::{self, user_exists},
+    //database::{self, user_exists},
 };
 
 #[get("/auth/validate")]
 pub async fn validate(cookies: &CookieJar<'_>) -> Custom<&'static str> {
-    match cookies.get_private("auth_key") {
-        Some(c) => {
-            if let Ok(s) = validate_jwt(c.value()).await {
-                let pool = database::connect_db().await;
-                if !user_exists(&s.user_at, &pool).await {
-                    return Custom(Status::Forbidden, "Invalid JSON Web Token");
-                }
-                Custom(Status::Ok, "Authorized")
-            } else {
-                Custom(Status::Forbidden, "Invalid JSON Web Token")
-            }
-        }
-        None => Custom(Status::Forbidden, "No credentials"),
-    }
+    let Some(c) = cookies.get_private("auth_key") else {
+        return Custom(Status::Forbidden, "No credentials");
+    };
+
+    let Ok(_s) = validate_jwt(c.value()).await else {
+        return Custom(Status::Forbidden, "Invalid JSON Web Token");
+    };
+
+    //let pool = database::connect_db().await;
+    //
+    //if !user_exists(&s.user_at, &pool).await {
+    //    return Custom(Status::Forbidden, "Invalid JSON Web Token");
+    //}
+    Custom(Status::Ok, "Authorized")
 }
